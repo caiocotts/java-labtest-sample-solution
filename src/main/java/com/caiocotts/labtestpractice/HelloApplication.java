ea2eaa4d3ed8d9386fb4b0159d482413b1167dde
Caio Cotts <caio@cotts.com.br>
2025-11-20 00:40:46 -0500

    simplify code

--- src/main/java/com/caiocotts/labtestpractice/HelloApplication.java
+++ src/main/java/com/caiocotts/labtestpractice/HelloApplication.java
@@ -19,39 +19,29 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) {
-
-        var gridPane = new GridPane();
         var pane = new Pane();
-        var hBox = new HBox();
+        var textFields = new HBox();
+        var buttons = new HBox();
         var vBox = new VBox();
 
-        var errorMessage = new Label();
-
-        hBox.getChildren().addAll(number1, number2);
-
-        GridPane.setConstraints(number1, 0, 0);
-        GridPane.setConstraints(number2, 1, 0);
+        textFields.getChildren().addAll(number1, number2);
 
         var add = new Button("+");
         add.setOnMouseClicked(_ -> calculate("+"));
-        GridPane.setConstraints(add, 0, 1);
 
         var subtract = new Button("-");
         subtract.setOnMouseClicked(_ -> calculate("-"));
-        GridPane.setConstraints(subtract, 1, 1);
 
         var multiply = new Button("*");
         multiply.setOnMouseClicked(_ -> calculate("*"));
-        GridPane.setConstraints(multiply, 2, 1);
 
         var divide = new Button("/");
         divide.setOnMouseClicked(_ -> calculate("/"));
-        GridPane.setConstraints(divide, 3, 1);
 
         GridPane.setConstraints(outputField, 0, 2);
 
-        gridPane.getChildren().addAll(add, subtract, multiply, divide);
-        vBox.getChildren().addAll(errorMessage, hBox, gridPane, outputField);
+        buttons.getChildren().addAll(add, subtract, multiply, divide);
+        vBox.getChildren().addAll(textFields, buttons, outputField);
         pane.getChildren().addAll(vBox);
 
         Scene scene = new Scene(pane);
